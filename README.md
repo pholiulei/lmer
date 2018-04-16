8e3ee75039c1b1f4d5f87effb66ec0db96886aa3
Trockly <wuguanfengyue123@163.com>
2018-04-16 19:04:12 +0800

    v1

---
+++ README.md
@@ -0,0 +1,5 @@
+# 说明
+mixed_effects_models_coding.R 为代码文件
+
+
+rdata.csv、rdata_L2.csv为数据文件
---
+++ mixed_effects_models_code.R
@@ -0,0 +1,46 @@
+#混合效应模型在语言研究中的应用代码
+
+install.packages("lme4")
+isntall.packages("lmerTest")
+library(lme4)
+library(lmerTest)
+rdata = read.csv("rdata.csv", header = T)
+rdata$subject = as.factor(rdata$subject)
+rdata$sentence = as.factor(rdata$sentence)
+rdata$attitude = as.factor(rdata$attitude)
+m1 = lmer(formula = pitchMean ~ attitude + (1 | subject) + (1 | sentence),
+          data = rdata)
+anova(m1)
+
+
+m2 = lmer(pitchMean ~ attitude + (1 + attitude | subject) + (1 + attitude | sentence),
+          data = rdata)
+anova(m2)
+
+
+anova(m1, m2)
+
+
+rdata_L2 = read.csv("rdata_L2.csv", header = T)
+rdata$subject = as.factor(rdata$subject)
+rdata$sentence = as.factor(rdata$sentence)
+rdata$group = as.factor(rdata$group)
+rdata$attitude = as.factor(rdata$attitude)
+m3 = lmer(pitchMean ~ group + attitude + group:attitude 
+          + (1 + attitude | subject) + (1 + group + attitude + group:attitude | sentence),
+          data = rdata_L2)
+anova(m3)
+
+
+install.packages("lsmeans")
+library(lsmeans)
+lsmeans(m3, pairwise ~ attitude, adjust = "tukey")
+lsmeans(m3, pairwise ~ group | attitude, adjust = "tukey")
+lsmeans(m3, pairwise ~ attitude | group, adjust = "tukey")
+
+
+install.packages("r2glmm")
+library(r2glmm)
+r2beta(m3, method = 'kr', partial = TRUE)
+
+
